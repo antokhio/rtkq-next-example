3095206872373e2aab791a2e025eec91468620c3
antokhio <antokhio@gmail.com>
2023-03-30 21:15:04 +0300

    init

--- src/store/api/api.ts
+++ src/store/api/api.ts
@@ -5,7 +5,7 @@ import type { AppState } from "..";
 export const api = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: `${process.env.NEXT_PUBLIC_API_URL}/api/v2`,
+    baseUrl: `https://pokeapi.co/api/v2`,
     // prepareHeaders: (headers, { getState }) => {
     //   const token = (getState() as AppState).user?.jwt;
     //   if (token) headers.set("authorization", `Bearer ${token}`);
